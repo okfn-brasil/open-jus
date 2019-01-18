fe3df295402f1e69a621accc7b8dcfddebd2272e
Eduardo Cuducos <cuducos@gmail.com>
2019-01-18 15:10:48 -0200

    Cleanup: remove placeholder test

--- justa/backend/justa/urls.py
+++ justa/backend/justa/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path
 
 urlpatterns = [
-    path('', lambda _: HttpResponse()),
     path('admin/', admin.site.urls),
 ]
