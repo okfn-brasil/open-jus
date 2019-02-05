0677a679736b08924fbf478d8e081b69ae73df5b
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2019-02-05 02:22:26 -0200

    Create BaseBudgetExecutionSpider
    This class will help implementing the spiders for other states,
    specially for those who uses ASP.NET ViewState (so the easiest approach
    will be to use splinter as in SP).

--- justa/crawler/budget_sp.py
+++ justa/crawler/budget_sp.py
@@ -1,56 +1,14 @@
 import io
 import time
-from collections import namedtuple
 
 import rows
-import rows.utils
-import splinter
+
+from budget_base import get_actions_for_state, BaseBudgetExecutionSpider, BRDecimalField
 
 
-Action = namedtuple("Action", ["year", "state", "name", "code"])
-
-
-class BRMoney(rows.fields.DecimalField):
-    @classmethod
-    def deserialize(self, value):
-        value = value.replace(".", "").replace(",", ".")
-        return super().deserialize(value)
-
-
-def get_actions_for_state(state):
-    url = "https://docs.google.com/spreadsheets/d/1epxFffymqv1t2s37rQ-p5eKpvecOIBzCfJPLI53wYTY/export?format=csv&id=1epxFffymqv1t2s37rQ-p5eKpvecOIBzCfJPLI53wYTY&gid=1565988556"
-    return [
-        Action(year=row.ano, state=row.estado, name=row.acao, code=row.cod_acao)
-        for row in rows.utils.import_from_uri(url)
-        if row.estado == state and all(row._asdict().values())
-    ]
-
-
-class SaoPauloBudgetExecutionScraper:
+class SaoPauloBudgetExecutionSpider(BaseBudgetExecutionSpider):
     url = "https://www.fazenda.sp.gov.br/SigeoLei131/Paginas/FlexConsDespesa.aspx"
-
-    def __init__(self):
-        self.__browser = None
-
-    @property
-    def browser(self):
-        if self.__browser is None:
-            self.__browser = splinter.Browser("chrome")
-        return self.__browser
-
-    def wait(self, sleep=0.1):
-        """Wait for the whole page to load - it takes time"""
-
-        finished = False
-        while not finished:
-            if len(self.browser.find_by_value("Pesquisar")) > 0:
-                finished = True
-            else:
-                time.sleep(sleep)
-
-    def start(self):
-        self.browser.visit(self.url)
-        self.wait()
+    value_to_wait_for = "Pesquisar"
 
     def select_year(self, year):
         select = self.browser.find_by_xpath("//select[contains(@name, 'Ano')]")[0]
@@ -88,20 +46,17 @@ class SaoPauloBudgetExecutionScraper:
         self.browser.find_by_value("Pesquisar")[0].click()
         self.wait()
 
-    def close(self):
-        self.browser.quit()
-
     def parse_budget(self):
         table = rows.import_from_html(
             io.BytesIO(self.browser.html.encode("utf-8")),
             index=10,
             force_types={
-                "dotacao_inicial": BRMoney,
-                "dotacao_atual": BRMoney,
-                "empenhado": BRMoney,
-                "liquidado": BRMoney,
-                "pago": BRMoney,
-                "pago_restos": BRMoney,
+                "dotacao_inicial": BRDecimalField,
+                "dotacao_atual": BRDecimalField,
+                "empenhado": BRDecimalField,
+                "liquidado": BRDecimalField,
+                "pago": BRDecimalField,
+                "pago_restos": BRDecimalField,
             },
         )
 
